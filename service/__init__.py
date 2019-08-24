538e45e74997fa4165b8d796f5942b0587803f30
Roman Detinin <detininroman@gmail.com>
2019-08-24 11:43:02 +0300

    changed structure; added get-handler for task 3

--- service/__init__.py
+++ service/__init__.py
@@ -1,14 +1,13 @@
 import shelve
+
 from flask import Flask, g, request
 from flask_restful import Resource, Api
-from datetime import datetime
+
+from service.tools import error, contains_digit, contains_letter, \
+    validate_payload as is_payload_invalid, debug
 
 app = Flask(__name__)
 api = Api(app)
-
-
-def debug(arg):
-    return print(arg, flush=True)
 
 
 def get_db():
@@ -16,93 +15,6 @@ def get_db():
     if db is None:
         db = g._database = shelve.open("devices.db")
     return db
-
-
-def error(arg):
-    return {'error': arg}
-
-
-def contains_digit(string):
-    return any(char.isdigit() for char in string)
-
-
-def contains_letter(string):
-    return any(char.isalpha() for char in string)
-
-
-def is_payload_invalid(citizens):
-    identifiers = list()
-    for citizen in citizens:
-
-        # check if all fields are present
-        if len(citizen.keys()) != 9:
-            return error('all fields must be filled'), 400
-
-        # check if all fields are not empty
-        for (key, value) in citizen.items():
-            if value is None:
-                return error(f'{key} must be specified'), 400
-
-        # validating citizen_id
-        identifiers.append(citizen['citizen_id'])
-        if citizen['citizen_id'] < 0:
-            return error('Citizen ID cannot be negative'), 400
-
-        # validating town
-        town = citizen['town']
-        if len(town) >= 256:
-            return error('Town name should not exceed 256 characters'), 400
-        if not (contains_digit(town) or contains_letter(town)):
-            return error('Town name should contain at least one letter or one digit'), 400
-
-        # validating street
-        street = citizen['street']
-        if len(street) >= 256:
-            return error('Street name should not exceed 256 characters'), 400
-        if not (contains_digit(street) or contains_letter(street)):
-            return error('Street name should contain at least one letter or one digit'), 400
-
-        # validating building
-        building = citizen['building']
-        if len(building) >= 256:
-            return error('Building name should not exceed 256 characters'), 400
-        if not (contains_digit(street) or contains_letter(street)):
-            return error('Building name should contain at least one letter or one digit'), 400
-
-        # validating apartment
-        if citizen['apartment'] < 0:
-            return error('Apartment number cannot be negative'), 400
-
-        # validating name
-        name = citizen['name']
-        if not len(name) or len(name) >= 256:
-            return error('Name should not be empty and exceed 256 characters'), 400
-
-        # validating birth_date
-        try:
-            birth_date = datetime.strptime(citizen['birth_date'], '%d.%m.%Y')
-            if birth_date >= datetime.now():
-                return error(f'Birth date `{birth_date}` is not valid'), 400
-        except ValueError:
-            return error('Birth date is not valid'), 400
-
-        # validating gender
-        if citizen['gender'] not in ['male', 'female']:
-            return error('Invalid gender'), 400
-
-        # validating relatives
-        if not isinstance(citizen['relatives'], list):
-            return error('Relatives field must be list'), 400
-
-        for relative in citizen['relatives']:
-            if not isinstance(relative, int):
-                return error('Relative must be int'), 400
-
-    # validate uniqueness
-    if not len(set(identifiers)) == len(identifiers):
-        return error('Identifiers are not unique'), 400
-
-    return
 
 
 @app.teardown_appcontext
@@ -115,13 +27,10 @@ def teardown_db(exception):
 class ImportList(Resource):
     def get(self):
         shelf = get_db()
-        print(shelf, flush=True)
         keys = list(shelf.keys())
 
         imports = []
-
         for key in keys:
-            print(shelf[key])
             imports.append(shelf[key])
 
         return {'imports': imports}, 200
@@ -153,4 +62,17 @@ class ImportList(Resource):
         return {'data': {'import_id': import_id}}, 201
 
 
+class Import(Resource):
+    def get(self, import_id):
+        shelf = get_db()
+        keys = list(shelf.keys())
+
+        for key in keys:
+            if shelf[key]['import_id'] == import_id:
+                return {'data': shelf[key]['citizens']}, 200
+
+        return error('not found'), 404
+
+
 api.add_resource(ImportList, '/imports')
+api.add_resource(Import, '/import/<int:import_id>/citizens')
