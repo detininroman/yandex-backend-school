414d3f479fba05de11e9eea41280daa02a59c767
Roman Detinin <detininroman@gmail.com>
2019-08-24 12:14:53 +0300

    structural changes

--- service/__init__.py
+++ service/__init__.py
@@ -1,13 +1,11 @@
 import shelve
 
 from flask import Flask, g, request
-from flask_restful import Resource, Api
 
 from service.tools import error, contains_digit, contains_letter, \
     validate_payload as is_payload_invalid, debug
 
 app = Flask(__name__)
-api = Api(app)
 
 
 def get_db():
@@ -24,55 +22,65 @@ def teardown_db(exception):
         db.close()
 
 
-class ImportList(Resource):
-    def get(self):
-        shelf = get_db()
-        keys = list(shelf.keys())
+@app.route('/imports', methods=['GET'])
+def get_all_imports():
+    shelf = get_db()
+    keys = list(shelf.keys())
 
-        imports = []
-        for key in keys:
-            imports.append(shelf[key])
+    imports = []
+    for key in keys:
+        imports.append(shelf[key])
 
-        return {'imports': imports}, 200
-
-    def post(self):
-        data = request.get_json()
-
-        invalid_payload = is_payload_invalid(data['citizens'])
-
-        if invalid_payload:
-            return invalid_payload
-
-        shelf = get_db()
-
-        # TODO: refactor it
-        keys = list(shelf.keys())
-        keysint = [int(i) for i in keys]
-        keysint.sort()
-        keys = [str(i) for i in keysint]
-
-        try:
-            import_id = dict(shelf[keys[-1]])['import_id'] + 1
-        except IndexError:
-            import_id = 1
-
-        data['import_id'] = import_id
-        shelf[str(import_id)] = data
-
-        return {'data': {'import_id': import_id}}, 201
+    return {'imports': imports}, 200
 
 
-class Import(Resource):
-    def get(self, import_id):
-        shelf = get_db()
-        keys = list(shelf.keys())
+@app.route('/imports', methods=['POST'])
+def post_import():
+    data = request.get_json()
 
-        for key in keys:
-            if shelf[key]['import_id'] == import_id:
-                return {'data': shelf[key]['citizens']}, 200
+    invalid_payload = is_payload_invalid(data['citizens'])
 
-        return error('not found'), 404
+    if invalid_payload:
+        return invalid_payload
+
+    shelf = get_db()
+
+    # TODO: refactor it
+    keys = list(shelf.keys())
+    keysint = [int(i) for i in keys]
+    keysint.sort()
+    keys = [str(i) for i in keysint]
+
+    try:
+        import_id = dict(shelf[keys[-1]])['import_id'] + 1
+    except IndexError:
+        import_id = 1
+
+    data['import_id'] = import_id
+    shelf[str(import_id)] = data
+
+    return {'data': {'import_id': import_id}}, 201
 
 
-api.add_resource(ImportList, '/imports')
-api.add_resource(Import, '/import/<int:import_id>/citizens')
+@app.route('/import/<int:import_id>/citizens', methods=['GET'])
+def get_import_citizens(import_id):
+    shelf = get_db()
+    keys = list(shelf.keys())
+
+    for key in keys:
+        if shelf[key]['import_id'] == import_id:
+            return {'data': shelf[key]['citizens']}, 200
+
+    return error('not found'), 404
+
+
+@app.route('/imports/<int:import_id>/citizens/<int:citizen_id>', methods=['PATCH'])
+def update_citizen(import_id, citizen_id):
+    shelf = get_db()
+    keys = list(shelf.keys())
+
+    for key in keys:
+        if shelf[key]['import_id'] == import_id:
+            return {'data': shelf[key]['citizens']}, 200
+
+    return error('not found'), 404
