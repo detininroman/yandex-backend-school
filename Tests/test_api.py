e6d182bbad5195a5f4468316545fd73e9afaa223
Roman Detinin <detininroman@gmail.com>
2019-08-25 23:01:29 +0300

    added invalid test

--- Tests/test_api.py
+++ Tests/test_api.py
@@ -3,7 +3,7 @@ import requests
 base_url = 'http://0.0.0.0:80'
 
 
-def test_create_get():
+def test_valid_create():
     citizens = list()
     for i in range(1, 4):
         citizens.append(
@@ -27,3 +27,25 @@ def test_create_get():
     response = requests.get(url=f'{base_url}/import/{import_id}/citizens')
     assert response.status_code == 200
     assert response.json()['data'] == payload['citizens']
+
+
+def test_invalid_create():
+    citizens = list()
+    for i in range(1, 4):
+        citizens.append(
+            dict(citizen_id="WRONG_CITIZEN_ID",
+                 town='Moscow',
+                 street='street_name',
+                 building='building_name',
+                 apartment=10,
+                 name='test_name',
+                 birth_date='03.02.1998',
+                 gender='male',
+                 relatives=[]
+                 )
+        )
+    payload = dict(citizens=citizens)
+
+    response = requests.post(url=f'{base_url}/imports', json=payload)
+    assert response.status_code == 400
+    assert response.json()['error'] == 'citizen_id name must be int'
